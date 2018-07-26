acc022a16d0fe207c5739cdf85ca4f5615b29fb5
yafei <yafei9@staff.sina.com.cn>
2018-07-26 15:27:34 +0800

    抓取赌博网站信息 v1

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+url_test = 'http://www.baidu.com/'
 
-url_wns = 'https://www.678922.cc/Lottery/syspk10List/?classid=109&chinese=极速赛车极速赛车'
+url_wns = 'https://www.678922.cc/Lottery/syspk10List/?classid=109&chinese=%E6%9E%81%E9%80%9F%E8%B5%9B%E8%BD%A6%E6%9E%81%E9%80%9F%E8%B5%9B%E8%BD%A6'
 target_wns = 500
 
 
--- wns.py
+++ wns.py
@@ -1,20 +1,37 @@
-#!/usr/bin/env python 
-# -*- coding:utf-8 -*-
+#!/usr/bin/env python
 
 
 import config
 import urllib.request
+# import http.cookiejar
 import re
+import parser
+import sys
+import codecs
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 
 url = config.url_wns
-response = urllib.request.urlopen(url)
-code = response.getcode()
-print(code)
+response = urllib.request.urlopen(url).read().decode('utf-8')
+# code = response.getcode()
+# print(response)
 
-# src="/Content/common/images/lottery/Images/Ball_4/10.png"
-#regex = '(src)="/Content/common/images/lottery/Images/Ball_4/"(\d+)'
-#matches = re.finditer(regex, response)
 
-print(response)
-#print(matches[0,10])
+
+regex = '/Content/common/images/lottery/Images/Ball_4/(\d+).png'
+matches = re.finditer(regex, response)
+
+print(matches)
+sum = 0
+ans = []
+temp = []
+for match in matches:
+    a = match.group(1)
+    print(a)
+    sum+=1
+    temp.append(a)
+    if sum%10 == 0 :
+        ans.append(temp)
+        print(temp)
+        temp  = []
+print(sum)

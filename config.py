7967eb26886b72b4578e4ba8c9848a206b0d0bdb
yafei <yafei9@staff.sina.com.cn>
2018-07-26 14:14:28 +0800

    修改

--- config.py
+++ config.py
@@ -2,3 +2,7 @@
 # -*- coding:utf-8 -*-
 
 
+url_wns = 'https://www.678922.cc/Lottery/syspk10List/?classid=109&chinese=极速赛车极速赛车'
+target_wns = 500
+
+
--- wns.py
+++ wns.py
@@ -2,3 +2,19 @@
 # -*- coding:utf-8 -*-
 
 
+import config
+import urllib.request
+import re
+
+
+url = config.url_wns
+response = urllib.request.urlopen(url)
+code = response.getcode()
+print(code)
+
+# src="/Content/common/images/lottery/Images/Ball_4/10.png"
+#regex = '(src)="/Content/common/images/lottery/Images/Ball_4/"(\d+)'
+#matches = re.finditer(regex, response)
+
+print(response)
+#print(matches[0,10])

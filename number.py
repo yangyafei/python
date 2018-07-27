1e083733eb59f4d0652932d7dabc2bce74474dc1
yafei <yafei9@staff.sina.com.cn>
2018-07-27 15:44:39 +0800

    number相关demo

--- number.py
+++ number.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
 
-
+import math
+import random
 
 
 print('pass 空语句，作为占位符')
@@ -80,7 +81,67 @@ tax = 1.5
 print(price * tax)
 # print('交互模式中，最后输出的表达式结果被赋值给变量_: ', _)
 
-# abs取绝对值
+# abs(x) 取绝对值
 print('abs取绝对值: ', abs(-10))
 
+# ceil(x) 向上取证 需要math模块
+print('ceil向上取整: ', math.ceil(18.1))
 
+# cmp(x,y) 比较大小 x<y,-1; x=y,0; x>y,1; python3已废弃，使用(x>y)-(x<y)替换
+
+# exp(x) 求e的x次幂 需要math
+print('exp求e的幂: ', math.exp(2))
+
+# fabs(x) 取绝对值
+print('fabs取绝对值: ', math.fabs(1.1))
+
+# floor(x) 向下取整
+print('floor向下取整: ', math.floor(1.6))
+
+# log(x) 取e的指数
+print('log取e的指数: ', math.log(2))
+
+# log10(x) 取10的指数
+print('log10取10的指数: ', math.log10(100))
+
+# max(v1, v2, ...) 参数可以是序列
+print('max取最大值: ', max([1,3,4,10]))
+
+# min(v1, v2, ...) 参数可以是序列
+print('min取最小值: ', min([10,3,-10]))
+
+# modf(x) 返回小数的整数和小数部分，2部分符号和x相同
+print('modf取浮点数的整数和小数: ', math.modf(2.34))
+
+# pow(x,y) x**y的运算
+print('pow 幂运算:', pow(2,3))
+
+# round(x,[n]) 浮点数四舍伍入
+print('round浮点数四舍五入: ', round(8.31264,3))
+
+# sqrt(x) 求平方跟
+print('sqrt求平方根:', math.sqrt(16))
+
+# choice(seq) 随机从序列取元素
+print('choice 随机从序列取元素: ', random.choice([1,3,4,5]))
+
+# randrange([start], stop, [step]) step递增基数
+print('randrange给定范围和递增基数取随机值 ', random.randrange(0, 100, 7))
+
+# random() 随机生成一个实数在[0,1)
+print('random 随机生成实数: ', random.random())
+
+# seed([x]) 改变种子生成器的种子seed
+#print('seed 改变种子生成器的种子seed:', random.seed())
+
+# shuffle(seq) 序列元素随机排序
+a = [13,4,1,5,131,5,66]
+random.shuffle(a)
+print('shuffle 序列元素随机排列: ', a)
+
+# uniform(x,y) [x,y]范围随机生成实数
+print('uniform随机生成范围内实数:', random.uniform(2,10))
+
+# pi
+
+print('累死啦 其他三角函数不写了!!!')
